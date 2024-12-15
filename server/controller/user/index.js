6acdea5b1c5ab7be70708762ca0e8614116d40a1
fiqrioemry <foemry@gmail.com>
2024-12-15 20:57:52 +0700

    add authentication middleware

--- server/controller/user/index.js
+++ server/controller/user/index.js
@@ -25,8 +25,53 @@ async function getUserProfile(req, res) {
 async function updateUserProfile(req, res) {
   try {
     const { userId } = req.user;
-    const { fullname } = req.body;
-  } catch (error) {}
+    const { fullname, phone, location, profession, opportunity, linkCV } =
+      req.body;
+
+    const oldUserData = await Profile.findOne({ userId });
+
+    if (!oldUserData) {
+      return res
+        .status(403)
+        .send({ success: false, message: "Unauthorized Access !!" });
+    }
+
+    const isDataUnchanged =
+      oldUserData.fullname === fullname &&
+      oldUserData.phone === phone &&
+      oldUserData.location === location &&
+      oldUserData.profession === profession &&
+      oldUserData.opportunity === opportunity &&
+      oldUserData.linkCV === linkCV;
+
+    if (isDataUnchanged) {
+      return res.status(400).send({
+        success: false,
+        message: "No changes detected. Please modify the data before updating.",
+      });
+    }
+
+    oldUserData.fullname = fullname;
+    oldUserData.phone = phone;
+    oldUserData.location = location;
+    oldUserData.profession = profession;
+    oldUserData.opportunity = opportunity;
+    oldUserData.linkCV = linkCV;
+
+    await oldUserData.save();
+
+    return res.status(200).send({
+      success: true,
+      message: "Profile is updated.",
+      data: oldUserData,
+    });
+  } catch (error) {
+    return res.status(500).send({
+      success: false,
+      message: "Internal Server Error",
+      error: error.message,
+    });
+  }
 }
 
 module.exports = { getUserProfile, updateUserProfile };
--- server/routes/user/index.js
+++ server/routes/user/index.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const isAuthenticate = require("../../middleware/isAuthenticate");
-const { getUserProfile } = require("../../controller/user/getUserProfile");
+const { getUserProfile } = require("../../controller/user");
 
 const router = express.Router();
 
 router.get("/profile", isAuthenticate, getUserProfile);
-router.put("/profile/update", isAuthenticate, getUserProfile);
+router.put("/profile/update", isAuthenticate, updateUserProfile);
 
 module.exports = router;
