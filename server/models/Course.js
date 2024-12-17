db78ebbbe80f2863bbc6a64bd93a37accbc467a2
fiqrioemry <foemry@gmail.com>
2024-12-17 22:54:38 +0700

    update changes

--- server/models/Course.js
+++ server/models/Course.js
@@ -1,5 +1,29 @@
 const mongoose = require("mongoose");
 
+const SubtopicSchema = new mongoose.Schema({
+  topicId: {
+    type: mongoose.Types.Schema.ObjectId,
+    ref: "Topic",
+    required: true,
+  },
+  name: String,
+  description: String,
+  videoUrl: String,
+  duration: String,
+});
+
+const TopicSchema = new mongoose.Schema({
+  courseId: {
+    type: mongoose.Types.Schema.ObjectId,
+    ref: "Course",
+    required: true,
+  },
+  name: { type: String, required: true },
+  instructor: String,
+  description: String,
+  subtopics: [SubtopicSchema],
+});
+
 const CourseSchema = new mongoose.Schema({
   name: { type: String, required: true },
   description: { type: String },
